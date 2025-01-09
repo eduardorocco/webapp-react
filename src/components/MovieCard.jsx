5af024c8dad2a8f9770225a40a5bfe2466762e9e
eduardorocco <eduardo_rocco@hotmail.it>
2025-01-09 15:56:38 +0100

    feat: add MovieCard component and implement movie fetching in HomePage

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,7 @@
+export default function MovieCard() {
+    return (
+        <div>
+            <h1>Movie card</h1>
+        </div>
+    )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,26 @@
+import { useEffct, useEffect, useState } from 'react';
+import axios from 'axios';
+const API_URL = 'http://localhost:3000/movies';
+
 export default function HomePage() {
+
+    const [movies, setMovies] = useState([]);
+
+    function fetchMovies() {
+        axios.get(API_URL)
+            .then(response => setMovies(response.data))
+    }
+
+    useEffect(() => { fetchMovies() }, []);
+
     return (
         <div>
             <h1>Home Page</h1>
+            <ul>
+                {movies.map(movie => (
+                    <li key={movie.id}>{movie.title}</li>
+                ))}
+            </ul>
         </div>
     )   
 }
