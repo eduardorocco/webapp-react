d7a4574c9724fb44c5a6dc15a472e5dd654b44b3
eduardorocco <eduardo_rocco@hotmail.it>
2025-01-10 11:44:34 +0100

    feat: implement movie fetching and display in MoviePage component

--- src/pages/movies/MoviePage.jsx
+++ src/pages/movies/MoviePage.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+const API_URL = 'http://localhost:3000/movies'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+
 export default function MoviePage() {
+
+    const { id } = useParams()
+    const [movie, setMovie] = useState({})
+
+    function fetchMovie() {
+        axios.get(`${API_URL}/${id}`).then(response => setMovie(response.data)
+    ).catch(error => console.error(error))
+    }
+
+    useEffect(() => { fetchMovie() }, [id])
+
+
     return (
-        <div>
-            <h1>movie page</h1>
-        </div>
+        <Container>
+            <Row>
+                <h1>{movie.title}</h1>
+                <img src={`http://localhost:3000/movies_cover/${movie.image}`} alt={movie.title} />
+                <p>{movie.abstract}</p>
+            </Row>
+        </Container>
     )   
 }
